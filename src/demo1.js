f7633b7e3965444fee71ff5aaa6aa7327707fc59
MeghaGajera2908 <meghagajera.dreamspot@gmail.com>
2023-12-04 17:48:00 -0800

    create-file

---
+++ src/demo1.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function MyFunction() {
+  return (
+    <div className="App">
+      <img alt='food' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiS8EOf9CFwW5b58OWHXoYQDX536ZJOc4oGw&usqp=CAU' />
+    </div>
+  );
+}
+
+export default MyFunction;
